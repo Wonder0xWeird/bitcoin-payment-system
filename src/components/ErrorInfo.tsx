6ffe653d1075e160f587b5f14bd065f597140144
w0nd3r <crawford.lars@gmail.com>
2025-05-29 11:22:08 +0100

    fix error component text

--- src/components/ErrorInfo.tsx
+++ src/components/ErrorInfo.tsx
@@ -36,7 +36,7 @@ export function ErrorInfo() {
 
         {/* Additional help text */}
         <p className="text-sm text-gray-500 dark:text-gray-400">
-          This will return you to the payment form to try again!
+          This will reset the application to try again!
         </p>
       </div>
     </div>
